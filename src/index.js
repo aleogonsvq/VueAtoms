cb7e741974b57ff540f27c743c9e81f9279c5987
Alberto Leon <aleogon@MacBook-Air-de-Alberto.local>
2026-03-26 20:38:42 +0100

    Dist hecha y libreria npm hecha

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import AtomArticle from "./components/atoms/AtomArticle.vue";
+import AtomAside from "./components/atoms/AtomAside.vue";
+import AtomButton from "./components/atoms/AtomButton.vue";
+import AtomCard from "./components/atoms/AtomCard.vue";
+import AtomCodeBlock from "./components/atoms/AtomCodeBlock.vue";
+import AtomDialog from "./components/atoms/AtomDialog.vue";
+import AtomFooter from "./components/atoms/AtomFooter.vue";
+import AtomForm from "./components/atoms/AtomForm.vue";
+import AtomH1 from "./components/atoms/AtomH1.vue";
+import AtomH2 from "./components/atoms/AtomH2.vue";
+import AtomHeader from "./components/atoms/AtomHeader.vue";
+import AtomImage from "./components/atoms/AtomImage.vue";
+import AtomInput from "./components/atoms/AtomInput.vue";
+import AtomLabel from "./components/atoms/AtomLabel.vue";
+import AtomMain from "./components/atoms/AtomMain.vue";
+import AtomNav from "./components/atoms/AtomNav.vue";
+import AtomSection from "./components/atoms/AtomSection.vue";
+import AtomTable from "./components/atoms/AtomTable.vue";
+import AtomTd from "./components/atoms/AtomTd.vue";
+import AtomTh from "./components/atoms/AtomTh.vue";
+import AtomTr from "./components/atoms/AtomTr.vue";
+import AtomUl from "./components/atoms/AtomUl.vue";
+
+export {
+    AtomArticle,
+    AtomAside,
+    AtomButton,
+    AtomCard,
+    AtomCodeBlock,
+    AtomDialog,
+    AtomFooter,
+    AtomForm,
+    AtomH1,
+    AtomH2,
+    AtomHeader,
+    AtomImage,
+    AtomInput,
+    AtomLabel,
+    AtomMain,
+    AtomNav,
+    AtomSection,
+    AtomTable,
+    AtomTd,
+    AtomTh,
+    AtomTr,
+    AtomUl
+}
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,28 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  plugins: [vue()],
+  build: {
+    // Activamos el Modo Librería
+    lib: {
+      // Le decimos cuál es el archivo principal que creamos en el Paso 1
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'MisAtomosUI', // Nombre global de la variable
+      fileName: (format) => `mis-atomos-ui.${format}.js`
     },
-  },
+    rollupOptions: {
+      // ¡MUY IMPORTANTE! Asegurarnos de externalizar Vue.
+      // No queremos empaquetar todo el código de Vue dentro de nuestra librería,
+      // porque el proyecto de destino ya tendrá su propio Vue instalado.
+      external: ['vue'],
+      output: {
+        // Proporcionamos variables globales para las dependencias externalizadas
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
